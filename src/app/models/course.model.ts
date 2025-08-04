254334f06d0b5d1bfb998b377ee08535e77cf28c
Haseebm01 <haseebdr01@gmail.com>
2025-08-04 14:18:36 -0700

    modified

---
+++ src/app/models/course.model.ts
@@ -0,0 +1,11 @@
+export interface Course {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    date?: string;
+    img?: string;
+    soldOut?: boolean;
+    onSale?: boolean;
+
+}
