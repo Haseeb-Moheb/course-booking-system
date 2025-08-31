484fe8dc3e0382e4e0917f69658d26a3106b19b7
Haseebm01 <haseebdr01@gmail.com>
2025-08-30 18:28:40 -0700

    modified

---
+++ src/app/add-new-course/add-new-course.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-new-course',
+  imports: [],
+  templateUrl: './add-new-course.component.html',
+  styleUrl: './add-new-course.component.css'
+})
+export class AddNewCourseComponent {
+
+}
